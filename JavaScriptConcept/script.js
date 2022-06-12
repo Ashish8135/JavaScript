ccf81674affb1308129db7e7bc7ab3ff92398a5a
ashish.shaivya10@gmail.com <ashish.shaivya10@gmail.com>
2022-06-12 22:28:25 +0530

    JavaScript Tutorials if else statment

--- JavaScriptConcept/script.js
+++ JavaScriptConcept/script.js
@@ -1,7 +1,33 @@
-var a;
-a = 5;
-//THis is a single comment
-/*
-THis is a multiple value comment 
-*/
-alert(a);
+// var a;
+// a = 5;
+// //THis is a single comment
+// /*
+// THis is a multiple value comment 
+// */
+// alert(a);
+// let x;
+// x=5;
+// let y =20;
+// let z;
+// z = x + y **y;
+// console.log(z);
+// alert(z);
+
+// If condition
+
+let age = 43;
+let hasvotercard = 'no'
+if(age>=18){
+    if(hasvotercard=="Yes"){
+        alert("You are eligile to vote");
+    }
+    else{
+        alert("Please get your voter id card.")
+    }    
+}
+// else if(age>=18 && hasvotercard!='Yes'){
+//     alert("Please get your voter idcard.")
+// }
+else{
+    alert("You are not eligible for vote.")
+}
