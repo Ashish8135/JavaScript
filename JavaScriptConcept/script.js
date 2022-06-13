2e21db36328551b0d68c9ac74cf8770ee138e42c
ashish.shaivya10@gmail.com <ashish.shaivya10@gmail.com>
2022-06-13 08:31:01 +0530

    alert,prompt,confirm completed in javascript

--- JavaScriptConcept/script.js
+++ JavaScriptConcept/script.js
@@ -37,88 +37,88 @@ else{
 
 
 
-let isloggedin = 0; 
-// if 0 loggedout if 1 loogedin
-if(isloggedin==0){
-    document.write("Please Login!")
-}
-else{
-    document.write("Logout")
-}
+// let isloggedin = 0; 
+// // if 0 loggedout if 1 loogedin
+// if(isloggedin==0){
+//     document.write("Please Login!")
+// }
+// else{
+//     document.write("Logout")
+// }
 
-// Ternary Operator
-let option = isloggedin == 1 ? "Logout" : "Login";
-document.write(option)
-alert(option)
+// // Ternary Operator
+// let option = isloggedin == 1 ? "Logout" : "Login";
+// document.write(option)
+// alert(option)
 
-// Switch Statement
+// // Switch Statement
 
-let input;
-input ='y';
-if(input===1){
-    document.write("Continue. .....")
-}
-else if(input==='y'){
-    document.write("Continue. .....")
-}
-else if(input==='yes'){
-    document.write("Continue. .....")
-}
-else if(input===0){
-    document.write("End. .....")
-}
-else if(input==='n'){
-    document.write("End. .....")
-}
-else if(input==='no'){
-    document.write("End .....")
-}
-else{
-    document.write("Wrong Input .....")
-}
+// let input;
+// input ='y';
+// if(input===1){
+//     document.write("Continue. .....")
+// }
+// else if(input==='y'){
+//     document.write("Continue. .....")
+// }
+// else if(input==='yes'){
+//     document.write("Continue. .....")
+// }
+// else if(input===0){
+//     document.write("End. .....")
+// }
+// else if(input==='n'){
+//     document.write("End. .....")
+// }
+// else if(input==='no'){
+//     document.write("End .....")
+// }
+// else{
+//     document.write("Wrong Input .....")
+// }
 
-// switch statement
+// // switch statement
 
-switch(input){
-    case 1: //if(input===1)
-        document.write("Continue...")
-        break;
-    case "y": //if(input==='y')
-        document.write("Continue...")
-        break;
-    case "yes": //if(input==='yes')
-        document.write("Continue...")
-        break;
-    case 0: //if(input===0)
-        document.write("No...")
-        break;
-    case "n": //if(input==='n')
-        document.write("No...")
-        break;
-    case "no": //if(input==='no')
-        document.write("No...")
-        break;
-    default :
-        document.write("Wrong Input .....")
-}
+// switch(input){
+//     case 1: //if(input===1)
+//         document.write("Continue...")
+//         break;
+//     case "y": //if(input==='y')
+//         document.write("Continue...")
+//         break;
+//     case "yes": //if(input==='yes')
+//         document.write("Continue...")
+//         break;
+//     case 0: //if(input===0)
+//         document.write("No...")
+//         break;
+//     case "n": //if(input==='n')
+//         document.write("No...")
+//         break;
+//     case "no": //if(input==='no')
+//         document.write("No...")
+//         break;
+//     default :
+//         document.write("Wrong Input .....")
+// }
 
 
 // 
 
-switch(input){
-    case 1: //if(input===1)
-    case "y": //if(input==='y')
-    case "yes": //if(input==='yes')
-        document.write("Continue...")
-        break;
-    case 0: //if(input===0)
-    case "n": //if(input==='n')
-    case "no": //if(input==='no')
-        document.write("No...")
-        break;
-    default :
-        document.write("Wrong Input .....")
-}
+// switch(input){
+//     case 1: //if(input===1)
+//     case "y": //if(input==='y')
+//     case "yes": //if(input==='yes')
+//         document.write("Continue...")
+//         break;
+//     case 0: //if(input===0)
+//     case "n": //if(input==='n')
+//     case "no": //if(input==='no')
+//         document.write("No...")
+//         break;
+//     default :
+//         document.write("Wrong Input .....")
+// }
 
 // Looping 
 //  let counter = 0;
@@ -127,37 +127,100 @@ switch(input){
 //     counter++;
 //  }
 
- let counter = 0;
- while(counter <=10){
-    counter++;
- }
- document.write("FresherHire");
+//  let counter = 0;
+//  while(counter <=10){
+//     counter++;
+//  }
+//  document.write("FresherHire");
 
 
-let count = 1;
-let sum = 0;
-while(count <=100){
-    if(count%2==0){
-        sum = sum + count;
-    }
-    count++;
-}
-document.write(sum,"  ")
+// let count = 1;
+// let sum = 0;
+// while(count <=100){
+//     if(count%2==0){
+//         sum = sum + count;
+//     }
+//     count++;
+// }
+// document.write(sum,"  ")
 
 
 // Do while loop
 // atleast one bar print krega
-let counting = 12
-do {
-    document.write("FresherHire Do While Demo");
-    counting++;
-}
-while (counting<=10){
-}
+// let counting = 12
+// do {
+//     document.write("FresherHire Do While Demo");
+//     counting++;
+// }
+// while (counting<=10){
+// }
 
 // For loop
 
+// for(let counted = 0; counted<=10;counted++){
+//     document.write("Welcome to FresherHIre",'<br>')
+// }
+// document.write("Welcome to FresherHIre");
+
+// 
+/*
 for(let counted = 0; counted<=10;counted++){
-    // document.write("Welcome to FresherHIre");
+    if(counted==5){
+        break
+    }
+    document.write("Welcome to FresherHIre",'<br>');
 }
-document.write("Welcome to FresherHIre");
+*/
+
+// Nested loop
+/*
+for(let counted = 0; counted<=10;counted++){
+    document.write(counted,'<br>');
+    for(let counter2 = 0;counter2<3;counter2++){
+        if (counter2==2){
+            break;
+        }
+        document.write("Nested Loop Statement",'<br><br>');
+    }
+}
+*/
+
+// Level loop
+/*
+outerloop:for(let counted = 0; counted<=10;counted++){
+    document.write(counted,'<br>');
+    for(let counter2 = 0;counter2<3;counter2++){
+        if (counter2==2){
+            break outerloop;
+        }
+        document.write("Nested Loop Statement",'<br><br>');
+    }
+}*/
+
+
+// Alert, Prompt and Confirm Statement
+
+// Alert() :- shows a message
+// prompt() :- shows a message,input text.It return  the text on OK or not.If cancle button or ESCis clicked,null.
+// Confirm() :- shows a message, Confirm or 'OK' or 'Cancle'.It return true  for ok and false for cancle and Esc.
+
+
+// alert("Welcome to FresherHire!")
+
+// let age = prompt('Enter Your Age : ');
+// if(age != null){
+//     console.log(`You age is ${age}`);
+//     document.write(`You age is ${age}`);
+// }
+// else{
+//     console.log('Age field was blank');
+//     document.write('Age field was blank');
+// }
+
+let response = confirm('Are you sure,You want to delete your account ?');
+if(response){
+    document.write('Account Deleted successfully.');
+}
+else{
+    document.write('Not deleted');
+}
